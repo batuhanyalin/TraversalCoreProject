f2b81733ff9b71e246dfd02bbb3f32b0f0ff4bf1
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-09-14 20:14:42 +0300

    Ek entityler oluşturuldu.

--- TraversalCoreProject.EntityLayer/Concrete/About2.cs
+++ TraversalCoreProject.EntityLayer/Concrete/About2.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace TraversalCoreProject.EntityLayer.Concrete
 {
-    public class About
+    public class About2
     {
-        public int AboutId { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string ImageUrl1 { get; set; }
+        public int About2Id { get; set; }
+        public string Title1 { get; set; }
         public string Title2 { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
         public bool Status { get; set; }
     }
 }
---
+++ TraversalCoreProject.EntityLayer/Concrete/Guide.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.EntityLayer.Concrete
+{
+    public class Guide
+    {
+        public int GuideId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Description { get; set; }
+        public string MainLanguage {  get; set; } 
+        public string OtherLanguage {  get; set; } 
+        public string ImageUrl {  get; set; } 
+        public string InstagramUrl {  get; set; } 
+        public string TwitterUrl {  get; set; } 
+        public DateTime Birtday { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
