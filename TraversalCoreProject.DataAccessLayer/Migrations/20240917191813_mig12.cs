c2de5afac85975dc80be789a7889d9a52a518bdd
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-09-17 22:18:31 +0300

    Destination entitysine eklemeler yapıldı.

---
+++ TraversalCoreProject.DataAccessLayer/Migrations/20240917191813_mig12.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TraversalCoreProject.DataAccessLayer.Migrations
+{
+    public partial class mig12 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CoverImage",
+                table: "Destinations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl2",
+                table: "Destinations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Text1",
+                table: "Destinations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Text2",
+                table: "Destinations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description2",
+                table: "Abouts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LittleTitle",
+                table: "Abouts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CoverImage",
+                table: "Destinations");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl2",
+                table: "Destinations");
+
+            migrationBuilder.DropColumn(
+                name: "Text1",
+                table: "Destinations");
+
+            migrationBuilder.DropColumn(
+                name: "Text2",
+                table: "Destinations");
+
+            migrationBuilder.DropColumn(
+                name: "Description2",
+                table: "Abouts");
+
+            migrationBuilder.DropColumn(
+                name: "LittleTitle",
+                table: "Abouts");
+        }
+    }
+}
