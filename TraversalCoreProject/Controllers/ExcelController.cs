302ecf9a5db8d27343b7af5228ea1e0b09e7b379
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-09-24 13:53:36 +0300

    Excel dinamik rapor alınan sistem oluşturuldu.

--- TraversalCoreProject/Controllers/ExcelController.cs
+++ TraversalCoreProject/Controllers/ExcelController.cs
@@ -1,4 +1,5 @@
 ﻿using ClosedXML.Excel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OfficeOpenXml;
 using TraversalCoreProject.DataAccessLayer.Context;
@@ -6,14 +7,35 @@ using TraversalCoreProject.Models;
 
 namespace TraversalCoreProject.Controllers
 {
+    [AllowAnonymous]
     public class ExcelController : Controller
     {
         TraversalContext context = new TraversalContext();
+
         public IActionResult Index()
         {
-            ExcelPackage excel=new ExcelPackage();
-            var workSheet=excel.Workbook.Worksheets.Add("Sayfa1");
-            workSheet.Cells[1,1].Value="Rota";
+            return View();
+        }
+
+        public List<DestinationReportModel> DestinationList()
+        {
+            List<DestinationReportModel> destinationReportModels = new List<DestinationReportModel>();
+            destinationReportModels = context.Destinations.Select(x => new DestinationReportModel
+            {
+                City = x.City,
+                DayNight = x.DayNight,
+                Price = x.Price,
+                Country = x.Country,
+                Capacity = x.Capacity,
+            }).ToList();
+            return destinationReportModels;
+        }
+
+        public IActionResult StaticExcelReport()
+        {
+            ExcelPackage excel = new ExcelPackage();
+            var workSheet = excel.Workbook.Worksheets.Add("Sayfa1");
+            workSheet.Cells[1, 1].Value = "Rota";
             workSheet.Cells[1, 2].Value = "Rehber";
             workSheet.Cells[1, 3].Value = "Kontenjan";
 
@@ -29,26 +51,38 @@ namespace TraversalCoreProject.Controllers
             workSheet.Cells[4, 2].Value = "Fatih ALTAYLI";
             workSheet.Cells[4, 3].Value = "38";
 
-            var bytes= excel.GetAsByteArray();
+            var bytes = excel.GetAsByteArray();
 
-            return File(bytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet","dosya1.xlsx");
+            return File(bytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "dosya1.xlsx");
         }
-        public List<DestinationReportModel> DestinationList()
-        {
-            List<DestinationReportModel> destinationReportModels = new List<DestinationReportModel>();
-          destinationReportModels=context.Destinations.Select(x=>new DestinationReportModel
-          {
-              City = x.City,
-              DayNight = x.DayNight,
-              Price = x.Price,
-              Country = x.Country,
-              Capacity = x.Capacity,
-          }).ToList();
-            return destinationReportModels;
-        }
+
         public IActionResult DestinationExcelReport()
         {
-            using(var workbook= new XLWorkbook)
+            using (var workbook = new XLWorkbook())
+            {
+                var workSheet = workbook.Worksheets.Add("Tur Listesi");
+                workSheet.Cell(1, 1).Value = "Ülke";
+                workSheet.Cell(1, 2).Value = "Şehir";
+                workSheet.Cell(1, 3).Value = "Konaklama Süresi";
+                workSheet.Cell(1, 4).Value = "Kapasite";
+                workSheet.Cell(1, 5).Value = "Fiyat";
+                int rowCount = 2;
+                foreach (var item in DestinationList())
+                {
+                    workSheet.Cell(rowCount, 1).Value = item.Country;
+                    workSheet.Cell(rowCount, 2).Value = item.City;
+                    workSheet.Cell(rowCount, 3).Value = item.DayNight;
+                    workSheet.Cell(rowCount, 4).Value = item.Capacity;
+                    workSheet.Cell(rowCount, 5).Value = item.Price;
+                    rowCount++;
+                }
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "yeniListe.xlsx");
+                }
+            }
         }
     }
 }
