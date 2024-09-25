d9b5f364cbb083d52e14c87578de5f837e679900
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-09-25 17:03:11 +0300

    AJAX işlemleri için controller oluşturuldu ve ajax ile veri listelemesi yapıldı.

---
+++ TraversalCoreProject/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TraversalCoreProject.Models;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]")]
+    public class CityController : Controller
+    {
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("CityList")]
+        public IActionResult CityList()
+        {
+            var jsonCity= JsonConvert.SerializeObject(cities);
+            return Json(jsonCity);
+        }
+
+        public static List<CityClass> cities = new List<CityClass>
+        {
+           new CityClass
+            {
+            CityId = 1,
+            CityName="Üsküp",
+            CityCountry="Makedonya"
+            },
+           new CityClass
+            {
+             CityId = 2,
+            CityName="Roma",
+            CityCountry="İtalya"
+            },
+
+           new CityClass
+            {
+             CityId = 3,
+            CityName="Londra",
+            CityCountry="İngiltere"
+            }
+        };
+    }
+}
