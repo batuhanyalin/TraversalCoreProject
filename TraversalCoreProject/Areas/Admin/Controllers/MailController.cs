94f687ed5b1e8bbda266b86470a39a9420a0132a
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-09-25 14:50:34 +0300

    Mail gönderme sayfası oluşturuldu ve mail gönderimi sağlandı.

--- TraversalCoreProject/Areas/Admin/Controllers/MailController.cs
+++ TraversalCoreProject/Areas/Admin/Controllers/MailController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
 using TraversalCoreProject.Models;
 
 namespace TraversalCoreProject.Areas.Admin.Controllers
@@ -8,13 +10,33 @@ namespace TraversalCoreProject.Areas.Admin.Controllers
     public class MailController : Controller
     {
         [HttpGet]
+        [Route("SendMail")]
         public IActionResult SendMail()
         {
             return View();
         }
-        [HttpGet]
+        [HttpPost]
+        [Route("SendMail")]
         public IActionResult SendMail(MailRequest mailRequest)
         {
+            mailRequest.SendingTime = DateTime.Now;
+            //MimeMessage kitinden bir nesne oluşturup bu nesneyi buna ekliyoruz.
+            MimeMessage mimeMessage = new MimeMessage();
+            //MimeKit sınıfından nesne türetiliyor ve gönderici bilgileri bu nesneye dolduruluyor.
+            MailboxAddress mailboxAddressSender = new MailboxAddress("Traversal Core Project - Admin", "traversalcoreproject@batuhanyalin.com");
+            mimeMessage.From.Add(mailboxAddressSender);
+
+            //Alıcıyı burada tekrar aynı sınıfla oluşturuyoruz.
+            MailboxAddress mailboxAddressReceiver = new MailboxAddress(mailRequest.ReceiverName, mailRequest.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressReceiver);//To ile alıcıyı seçiyoruz.
+            mimeMessage.Subject=mailRequest.Subject;
+            mimeMessage.Date = mailRequest.SendingTime;
+            //MailKit.Net.Smtp ile SmtpClient sınıfından nesne türetiyoruz.
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Connect("mail.batuhanyalin.com",587,false);
+            smtpClient.Authenticate("traversalcoreproject@batuhanyalin.com", "Vfv;apmH~$uU");
+            smtpClient.Send(mimeMessage);
+            smtpClient.Disconnect(true);
             return View();
         }
     }
--- TraversalCoreProject/Models/MailRequest.cs
+++ TraversalCoreProject/Models/MailRequest.cs
@@ -2,7 +2,9 @@
 {
     public class MailRequest
     {
+        public string SenderName { get; set; }
         public string SenderMail { get; set; }
+        public string ReceiverName { get; set; }
         public string ReceiverMail { get; set; }
         public string Subject { get; set; }
         public string Text { get; set; }
