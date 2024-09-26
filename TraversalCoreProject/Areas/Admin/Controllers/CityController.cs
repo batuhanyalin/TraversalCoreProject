5b47c5abdbc1f97a5282a2a8a76e48e1709e19e9
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-09-26 15:09:50 +0300

    AJAXla tur ekleme işlemi başarıyla yapılacak şekilde oluşturuldu.

--- TraversalCoreProject/Areas/Admin/Controllers/CityController.cs
+++ TraversalCoreProject/Areas/Admin/Controllers/CityController.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using TraversalCoreProject.BusinessLayer.Abstract;
+using TraversalCoreProject.EntityLayer.Concrete;
 using TraversalCoreProject.Models;
 
 namespace TraversalCoreProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
     [Route("Admin/[controller]")]
+    [AllowAnonymous]
     public class CityController : Controller
     {
+        private readonly IDestinationService _destinationService;
+
+        public CityController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
         [Route("Index")]
         public IActionResult Index()
         {
@@ -17,31 +28,50 @@ namespace TraversalCoreProject.Areas.Admin.Controllers
         [Route("CityList")]
         public IActionResult CityList()
         {
-            var jsonCity= JsonConvert.SerializeObject(cities);
+            var values= _destinationService.TGetListAll();
+            var jsonCity= JsonConvert.SerializeObject(values);
             return Json(jsonCity);
         }
-
-        public static List<CityClass> cities = new List<CityClass>
+        [HttpPost]
+        [Route("AddDestination")]
+        public async Task<IActionResult> AddDestination(Destination destination)
         {
-           new CityClass
-            {
-            CityId = 1,
-            CityName="Üsküp",
-            CityCountry="Makedonya"
-            },
-           new CityClass
-            {
-             CityId = 2,
-            CityName="Roma",
-            CityCountry="İtalya"
-            },
+            destination.Status = true;
+            destination.StartDate = DateTime.Now;
+            destination.ImageUrl = "ImageUrl";
+            destination.CoverImage = "CoverImageUrl";
+            destination.Text1 = "text1";
+            destination.Detail = "detail";
+            destination.Description = "description";
+            destination.IsFeaturePost = false;
+           _destinationService.TInsert(destination);
+            var values = JsonConvert.SerializeObject(destination);
+            return Json(values);
+        }
+       
 
-           new CityClass
-            {
-             CityId = 3,
-            CityName="Londra",
-            CityCountry="İngiltere"
-            }
-        };
+
+        //public static List<CityClass> cities = new List<CityClass>
+        //{
+        //   new CityClass
+        //    {
+        //    CityId = 1,
+        //    CityName="Üsküp",
+        //    CityCountry="Makedonya"
+        //    },
+        //   new CityClass
+        //    {
+        //     CityId = 2,
+        //    CityName="Roma",
+        //    CityCountry="İtalya"
+        //    },
+
+        //   new CityClass
+        //    {
+        //     CityId = 3,
+        //    CityName="Londra",
+        //    CityCountry="İngiltere"
+        //    }
+        //};
     }
 }
