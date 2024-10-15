9365a0be18825875326020645acf211c4db36eca
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-10-15 18:24:35 +0300

    MemberNavbara özel dil seç tasarımı yapıldı.

---
+++ TraversalCoreProject/Areas/Member/ViewComponents/_MemberNavbarLanguageComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Member.ViewComponents
+{
+    public class _MemberNavbarLanguageComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
